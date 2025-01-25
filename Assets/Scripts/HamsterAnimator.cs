bfe60cd133a3ad5e7babf9a1e9e915c4cf1a341d
Sjouke IJntema <18188660+DevCas1@users.noreply.github.com>
2025-01-25 11:06:58 +0100

    Adjust movement parameters | Make hamster-object rotate in input direction | Make hamster-ball transparent, and add inside-ball with backface rendering

---
+++ Assets/Scripts/HamsterAnimator.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HamsterAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 1;
+
+    private Vector2 _inputVector;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(_inputVector.x, 0, _inputVector.y));
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            Time.deltaTime * _rotationSpeed
+        );
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 input = movementValue.Get<Vector2>();
+
+        if (input.sqrMagnitude > 0)
+            _inputVector = input;
+    }
+}
